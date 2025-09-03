3f8a4a5f7f5fda5a5c7bbfecb3ac5543a27ffcdd
adityakumarsingh8077 <singhaditya8077@gmail.com>
2025-09-03 19:52:47 +0530

    basic sturcture and navigation bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import "./app.scss"
+import Test from "./test.jsx";
+import Navbar from "./components/navbar/navbar.jsx";
+// import Sidebar from "./components/sidebar/Sidebar.jsx";
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage"><Navbar/>  
+    </section>
+    <section id="Services">Parallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+ 
+    {/* <Test/> */}
+
+
+
+
+    
+      </div>;
 };
 
 export default App;
