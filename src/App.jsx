0cc5903dd664c4baa682f3234d4e4275a9528d54
adityakumarsingh8077 <singhaditya8077@gmail.com>
2025-09-05 02:42:29 +0530

    services section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./app.scss"
 import Test from "./test.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
+import Parallax from "./components/parallax/Parallax.jsx";
+import Services from "./components/services/Services.jsx";
 // import Sidebar from "./components/sidebar/Sidebar.jsx";
 const App = () => {
   return <div>
@@ -9,9 +11,9 @@ const App = () => {
       <Navbar/>  
       <Hero/>
     </section>
-    <section id="Services">Parallax</section>
-    <section>Services</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section><Services/></section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
